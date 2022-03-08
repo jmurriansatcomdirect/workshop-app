b088e672556f80c63bfca193421769f681baa150
Jubal Murrian <jmurrian@satcomdirect.com>
2022-03-08 16:32:05 -0500

    workshop-app

--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Video } from 'src/app/types';
 
@@ -8,204 +10,19 @@ import { Video } from 'src/app/types';
 })
 export class VideoDashboardComponent implements OnInit {
 
-  public selectedVideo:Video | undefined;
+  public selectedVideo: Video | undefined;
+  public videoList: Video[] | undefined;
 
-  constructor() { }
+  constructor(public httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get('https://api.angularbootcamp.com/videos')
+      .subscribe(videos => (this.videoList = <Video[]>(videos)));
   }
 
-  public selectVideo(video:Video){
+  public selectVideo(video: Video) {
     this.selectedVideo = video;
   }
-
-  public videoList = [
-    {
-      "title": "Angular Observable Data Flow",
-      "author": "Kyle Cordes",
-      "id": "JPuqluYYa-o",
-      "viewDetails": [
-        {
-          "age": 17,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 27,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 37,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 47,
-          "region": "Europe",
-          "date": "2016-03-24"
-        },
-        {
-          "age": 37,
-          "region": "North America",
-          "date": "2016-03-24"
-        },
-        {
-          "age": 17,
-          "region": "North America",
-          "date": "2016-03-25"
-        }
-      ]
-    },
-    {
-      "title": "Angular Performance Checklist",
-      "author": "Paul Spears",
-      "id": "cxqRijt9LbQ",
-      "viewDetails": [
-        {
-          "age": 36,
-          "region": "North America",
-          "date": "2016-06-23"
-        },
-        {
-          "age": 30,
-          "region": "North America",
-          "date": "2016-06-23"
-        },
-        {
-          "age": 54,
-          "region": "North America",
-          "date": "2016-07-23"
-        },
-        {
-          "age": 43,
-          "region": "Europe",
-          "date": "2016-0-24"
-        },
-        {
-          "age": 32,
-          "region": "North America",
-          "date": "2016-08-24"
-        },
-        {
-          "age": 32,
-          "region": "North America",
-          "date": "2016-08-25"
-        }
-      ]
-    },
-    {
-      "title": "Live App Updates Without The App Store",
-      "author": "Sani Yusuf",
-      "id": "s10wrXA-a7Y",
-      "viewDetails": [
-        {
-          "age": 17,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 27,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 37,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 47,
-          "region": "Europe",
-          "date": "2016-03-24"
-        },
-        {
-          "age": 37,
-          "region": "North America",
-          "date": "2016-03-24"
-        },
-        {
-          "age": 17,
-          "region": "North America",
-          "date": "2016-03-25"
-        }
-      ]
-    },
-    {
-      "title": "Angular Reactive Forms",
-      "author": "Jack Balbes",
-      "id": "A_Rq6ZsoXpI",
-      "viewDetails": [
-        {
-          "age": 36,
-          "region": "North America",
-          "date": "2016-06-23"
-        },
-        {
-          "age": 30,
-          "region": "North America",
-          "date": "2016-06-23"
-        },
-        {
-          "age": 54,
-          "region": "North America",
-          "date": "2016-07-23"
-        },
-        {
-          "age": 43,
-          "region": "Europe",
-          "date": "2016-0-24"
-        },
-        {
-          "age": 32,
-          "region": "North America",
-          "date": "2016-08-24"
-        },
-        {
-          "age": 32,
-          "region": "North America",
-          "date": "2016-08-25"
-        }
-      ]
-    },
-    {
-      "title": "Imperative to Reactive with Angular and RxJS",
-      "author": "John Baur",
-      "id": "VJOPsjlbhdg",
-      "viewDetails": [
-        {
-          "age": 17,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 27,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 37,
-          "region": "North America",
-          "date": "2016-03-23"
-        },
-        {
-          "age": 47,
-          "region": "Europe",
-          "date": "2016-03-24"
-        },
-        {
-          "age": 37,
-          "region": "North America",
-          "date": "2016-03-24"
-        },
-        {
-          "age": 17,
-          "region": "North America",
-          "date": "2016-03-25"
-        }
-      ]
-    }
-  ];
 
 }
 
