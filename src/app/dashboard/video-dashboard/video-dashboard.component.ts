8ca36e590ba66ed87deac451a56563aa2c7b5c4a
Jubal Murrian <jmurrian@satcomdirect.com>
2022-03-08 16:42:05 -0500

    workshop-app

--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { ThisReceiver } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Video } from 'src/app/types';
 
 @Component({
@@ -8,16 +9,21 @@ import { Video } from 'src/app/types';
   templateUrl: './video-dashboard.component.html',
   styleUrls: ['./video-dashboard.component.css']
 })
-export class VideoDashboardComponent implements OnInit {
+export class VideoDashboardComponent implements OnInit,OnDestroy {
 
   public selectedVideo: Video | undefined;
   public videoList: Video[] | undefined;
+  private subscription:Subscription | undefined;
 
   constructor(public httpClient: HttpClient) { }
-
+  
   ngOnInit(): void {
-    this.httpClient.get('https://api.angularbootcamp.com/videos')
+    this.subscription = this.httpClient.get('https://api.angularbootcamp.com/videos')
       .subscribe(videos => (this.videoList = <Video[]>(videos)));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
   public selectVideo(video: Video) {
