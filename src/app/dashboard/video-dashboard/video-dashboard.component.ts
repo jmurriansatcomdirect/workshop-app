07c0ca75b8a7243cc648e35044e70ea8c52ead02
Jubal Murrian <jmurrian@satcomdirect.com>
2022-03-09 09:36:18 -0500

    workshop-app

--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -14,12 +14,17 @@ export class VideoDashboardComponent implements OnInit, OnDestroy {
   public selectedVideo: Video | undefined;
   public videoList: Video[] | undefined;
   private subscription: Subscription | undefined;
+  loading:boolean=false;
 
   constructor(public httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.loading=true;
     this.subscription = this.httpClient.get<Video[]>('https://api.angularbootcamp.com/videos')
-      .subscribe(videos => (this.videoList = videos));
+      .subscribe(videos => {
+        this.videoList = videos;
+        this.loading=false;
+      });
   }
 
   ngOnDestroy(): void {
