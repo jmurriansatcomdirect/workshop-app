d4d905e090106c19e090bdaf1eeb43bbc3227ffd
Jubal Murrian <jmurrian@satcomdirect.com>
2022-03-09 11:56:05 -0500

    workshop-app

--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { ThisReceiver } from '@angular/compiler';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subject, Subscription, takeUntil } from 'rxjs';
+import { VideoService } from 'src/app/service/video-service';
 import { Video } from 'src/app/types';
 
 @Component({
@@ -16,11 +16,11 @@ export class VideoDashboardComponent implements OnInit, OnDestroy {
   loading:boolean=false;
   private unsubscribe:Subject<any> = new Subject<any>();
   
-  constructor(public httpClient: HttpClient) { }
+  constructor(public videoService:VideoService) { }
 
   ngOnInit(): void {
     this.loading=true;
-    this.httpClient.get<Video[]>('https://api.angularbootcamp.com/videos')
+    this.videoService.getVideos()
     .pipe(takeUntil(this.unsubscribe))
       .subscribe(videos => {
         this.videoList = videos;
---
+++ src/app/service/video-service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Video } from '../types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideoService {
+
+  constructor(public httpClient: HttpClient) { }
+
+  public getVideos(): Observable<Video[]> {
+    return this.httpClient.get<Video[]>('https://api.angularbootcamp.com/videos');
+  }
+}
